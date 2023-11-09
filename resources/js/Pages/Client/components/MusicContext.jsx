17d8564febd4ac7fbb3ddb214f4ba69668f608a5
huyentrang2k2 <nguyentrang40901@gmail.com>
2023-11-09 13:22:46 +0700

    up-hidden

--- resources/js/Pages/Client/components/MusicContext.jsx
+++ resources/js/Pages/Client/components/MusicContext.jsx
@@ -1,4 +1,10 @@
-import React, { createContext, useContext, useReducer, useState } from "react";
+import React, {
+    createContext,
+    useContext,
+    useReducer,
+    useState,
+    useEffect,
+} from "react";
 
 const MusicContext = createContext();
 
@@ -22,7 +28,40 @@ export function MusicProvider({ children }) {
     const [state, dispatch] = useReducer(musicReducer, {
         currentSong: null,
     });
-    const [isMusicPlayerVisible, setIsMusicPlayerVisible] = useState(true);
+
+    const shouldHideMusicPlayer =
+        localStorage.getItem("hideMusicPlayer") === "true";
+
+    const [isMusicPlayerVisible, setIsMusicPlayerVisible] = useState(
+        !shouldHideMusicPlayer
+    );
+
+    const pagesToHideMusicPlayer = ["/dashboard", "/login", "/register"];
+
+    useEffect(() => {
+        // Lấy đường dẫn URL của trang hiện tại
+        const currentPath = window.location.pathname;
+
+        // Kiểm tra xem trang hiện tại có nằm trong danh sách pagesToHideMusicPlayer không
+        const shouldHide = pagesToHideMusicPlayer.includes(currentPath);
+        const shouldHided =
+            currentPath.startsWith("/user") ||
+            currentPath.startsWith("/music") ||
+            currentPath.startsWith("/categories") ||
+            currentPath.startsWith("/album");
+
+        // Cập nhật trạng thái và localStorage cho từng trường hợp
+        if (shouldHide) {
+            setIsMusicPlayerVisible(false);
+            localStorage.setItem("hideMusicPlayer", "true");
+        } else if (shouldHided) {
+            setIsMusicPlayerVisible(false);
+            localStorage.setItem("hideMusicPlayer", "true");
+        } else {
+            setIsMusicPlayerVisible(true);
+            localStorage.setItem("hideMusicPlayer", "false");
+        }
+    }, []);
 
     return (
         <MusicContext.Provider
