a0ff015ea6acc11a189f59357e25b6a0ab9221f5
huyentrang2k2 <nguyentrang40901@gmail.com>
2023-11-17 07:24:58 +0700

    nut-play

--- resources/js/Pages/Client/PlayList.jsx
+++ resources/js/Pages/Client/PlayList.jsx
@@ -49,25 +49,22 @@ export default function PlayList() {
                                         onClick={() => playMusic(item)}
                                         onMouseEnter={handleMouseEnter}
                                         onMouseLeave={handleMouseLeave}
-                                        className={`relative group ${
+                                        className={`hover:bg-gradient-to-t from-teal-950 ${
                                             item.isCurrent
                                                 ? "text-green-500"
                                                 : ""
                                         }`}
                                     >
-                                        <td>{index + 1}</td>
-                                        <td className="flex justify-center my-2">
-                                            <img
-                                                src={`http://localhost:8000/upload/images/${item.thumbnail}`}
-                                                alt=""
-                                                className="rounded-lg lg:w-20 lg:h-20 w-20 object-cover"
-                                            />
+                                        <td className="relative group">
+                                            <span className="group-hover:hidden">
+                                                {index + 1}
+                                            </span>
                                             {isHovered && (
                                                 <button
                                                     onClick={() =>
                                                         playMusic(item)
                                                     }
-                                                    className="hidden group-hover:block absolute top-1/2 left-14 transform -translate-x-1/2 -translate-y-1/2"
+                                                    className="hidden group-hover:block absolute top-1/2 left-1 transform -translate-x-1/2 -translate-y-1/2"
                                                 >
                                                     <svg
                                                         xmlns="http://www.w3.org/2000/svg"
@@ -75,23 +72,24 @@ export default function PlayList() {
                                                         viewBox="0 0 24 24"
                                                         strokeWidth={1.5}
                                                         stroke="currentColor"
-                                                        className="w-14 h-14 stroke-none"
+                                                        className="w-16 h-16 stroke-none"
                                                     >
                                                         <path
                                                             strokeLinecap="round"
                                                             strokeLinejoin="round"
-                                                            className="fill-green-600"
-                                                            d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                                                        />
-                                                        <path
-                                                            strokeLinecap="round"
-                                                            strokeLinejoin="round"
-                                                            className="fill-black"
+                                                            className="fill-green-500"
                                                             d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
                                                         />
                                                     </svg>
                                                 </button>
                                             )}
+                                        </td>
+                                        <td className="flex justify-center my-2">
+                                            <img
+                                                src={`http://localhost:8000/upload/images/${item.thumbnail}`}
+                                                alt=""
+                                                className="rounded-lg lg:w-20 lg:h-20 w-20 object-cover"
+                                            />
                                         </td>
                                         <td>
                                             <span>{item.name}</span>
