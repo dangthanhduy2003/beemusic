4168977198054dcc43f3798e82514ad7000e0610
huyentrang2k2 <nguyentrang40901@gmail.com>
2023-11-21 15:11:37 +0700

    up-dropdown

--- resources/js/Pages/Client/components/Header.jsx
+++ resources/js/Pages/Client/components/Header.jsx
@@ -1,7 +1,8 @@
 import { Link } from "@inertiajs/react";
 import { useMusic } from "./MusicContext";
+import Dropdown from "@/Components/Dropdown";
 
-export default function Header() {
+export default function Header({ auth }) {
     const { setIsMusicPlayerVisible } = useMusic();
 
     // Ẩn thanh phát nhạc
@@ -50,6 +51,47 @@ export default function Header() {
                         </svg>
                     </button>
                 </div>
+                <Dropdown>
+                    <Dropdown.Trigger>
+                        <img
+                            className="inline-block h-8 w-8 rounded-full ring-2 ring-white"
+                            src=""
+                            alt=""
+                        />
+                        <span className="inline-flex rounded-md">
+                            <button
+                                type="button"
+                                className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white hover:text-cyan-400 focus:outline-none transition ease-in-out duration-150"
+                            >
+                                <svg
+                                    className="ml-2 -mr-0.5 h-4 w-4"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </button>
+                        </span>
+                    </Dropdown.Trigger>
+                    <Dropdown.Content>
+                        <Dropdown.Link href={route("profile.edit")}>
+                            Hồ sơ
+                        </Dropdown.Link>
+                        <Dropdown.Link
+                            href={route("logout")}
+                            onClick={hideMusicPlayer}
+                            method="post"
+                            as="button"
+                        >
+                            Đăng xuất
+                        </Dropdown.Link>
+                    </Dropdown.Content>
+                </Dropdown>
                 <Link href="/dashboard" onClick={hideMusicPlayer}>
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
