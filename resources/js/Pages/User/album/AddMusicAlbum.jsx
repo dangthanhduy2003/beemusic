17856017c1c6ddbfd9ee5a64b08cb36761b7526b
Thanh Duy <thanhduy11a11@gmail.com>
2023-12-01 13:32:09 +0700

    up

--- resources/js/Pages/User/album/AddMusicAlbum.jsx
+++ resources/js/Pages/User/album/AddMusicAlbum.jsx
@@ -123,7 +123,7 @@ export default function AddMusicAlbum({
                                             <div className="flex flex-row gap-2">
                                                 <img
                                                     className="w-24 h-20 oject-cover"
-                                                    src={`../upload/images/${music.thumbnail}`}
+                                                    src={`../../upload/images/${music.thumbnail}`}
                                                     alt=""
                                                 />
                                                 <div className="flex flex-col">
