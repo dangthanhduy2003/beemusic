02f04cce08d8de90ad05d3112a00ba2f74266a6d
Thanh Duy <thanhduy11a11@gmail.com>
2023-12-26 20:44:40 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -196,6 +196,9 @@ Route::post('/view/{id}', [HomeController::class, 'updateView'])->name('view');
 Route::get('/home/listHome', [HomeAdminController::class, 'ListHome'])->name('home.listHome');
 //hiển thị bài nhạc của album
 Route::get('/home/listMusic/{id}', [HomeAdminController::class, 'listMusic'])->name('home.listMusic');
+Route::get('/home/update/{id}', [HomeAdminController::class, 'Update'])->name('home.up');
+Route::post('/home/update/{id}', [HomeAdminController::class, 'UpdateHome'])->name('home.update');
+//
 Route::post('/home/addMusicHome/{id}', [HomeAdminController::class, 'addMusicHome'])->name('home.addMusicHome');
 Route::get('/home/DeleteMusicHome/{id}/{id_home}', [HomeAdminController::class, 'DeleteMusicHome'])->name('home.DeleteMusicHome');
 require __DIR__ . '/auth.php';
